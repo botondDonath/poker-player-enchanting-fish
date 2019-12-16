a54d5904e7bfff8468b10ab74317d2ececfebd6e
botond <belgianmaniax@gmail.com>
2019-12-16 10:27:24 +0100

    Commit 2

--- player.py
+++ player.py
@@ -3,6 +3,10 @@ class Player:
     VERSION = "1.0"
 
     def betRequest(self, game_state):
+        player_index = game_state['in_action']
+        players = game_state['players']
+        player = players[player_index]
+
         community_cards = game_state['community_cards']
         min_raise = game_state['minimum_raise']
         if not community_cards:
