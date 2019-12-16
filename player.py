383b40088b01fe01a596129630adc1a9b8d6389d
botond <belgianmaniax@gmail.com>
2019-12-16 11:28:04 +0100

    Commit

--- player.py
+++ player.py
@@ -14,6 +14,8 @@ class Player:
         current_buy_in = game_state['current_buy_in']
         our_bet = players[player_index]['bet']
 
+        high_cards = card1['rank'] in ('J', 'K', 'Q', 'A') and card2['rank'] in ('J', 'K', 'Q', 'A')
+
         pair_in_hand = card1['rank'] == card2['rank']
         match_count = 1 if pair_in_hand else 0
         if community_cards:
@@ -22,11 +24,7 @@ class Player:
                     match_count += 1
 
         if not community_cards:
-            if (
-                    card1['rank'] in ('J', 'K', 'Q', 'A') and card2['rank'] in ('J', 'K', 'Q', 'A')
-            ) or (
-                    match_count and card1['rank'] not in (str(i) for i in range(1, 7))
-            ):
+            if high_cards or (match_count and card1['rank'] not in (str(i) for i in range(1, 7))):
                 return current_buy_in - our_bet + min_raise
 
             else:
