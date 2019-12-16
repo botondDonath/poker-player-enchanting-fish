746f0566c4819a8598f213821fe1e14840ef2829
botond <belgianmaniax@gmail.com>
2019-12-16 11:32:32 +0100

    Commit

--- player.py
+++ player.py
@@ -14,16 +14,25 @@ class Player:
         current_buy_in = game_state['current_buy_in']
         our_bet = players[player_index]['bet']
 
+        round_status_dict = {
+            0: 'preflop',
+            3: 'flop',
+            4: 'turn',
+            5: 'river'
+        }
+
+        round_status = round_status_dict[len(community_cards)]
+
         high_cards = card1['rank'] in ('J', 'K', 'Q', 'A') and card2['rank'] in ('J', 'K', 'Q', 'A')
 
         pair_in_hand = card1['rank'] == card2['rank']
         match_count = 1 if pair_in_hand else 0
-        if community_cards:
+        if round_status == 'preflop':
             for card in community_cards:
                 if card['rank'] in (card1['rank'], card2['rank']):
                     match_count += 1
 
-        if not community_cards:
+        elif round_status != 'preflop':
             if high_cards or (match_count and card1['rank'] not in (str(i) for i in range(1, 7))):
                 return current_buy_in - our_bet + min_raise
 
